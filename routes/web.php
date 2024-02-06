dedf86ba6cd91e427bf9dbc661d6acbb62f7d1b8
reyn-nova <rynvva@gmail.com>
2024-02-06 09:52:12 +0700

    feat: update login and register page

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('main');
+});
+
+Route::get('/register', function () {
+    return view('register');
 });
